b12f779c4e4b5d98c7e4f01bd317d9631998d048
imtahirabatool <btahira183@gmail.com>
2024-04-13 19:38:37 +0500

    server setup

--- server/node_modules/country-state-city/lib/cjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Country from './country';
-import State from './state';
-import City from './city';
-import { ICountry, IState, ICity } from './interface';
-export { Country };
-export { State };
-export { City };
-export { ICountry, IState, ICity };
