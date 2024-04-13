2c4ec4700477bd9647328099d3081a74d28723fc
imtahirabatool <btahira183@gmail.com>
2024-04-13 23:21:02 +0500

    Half backend setup complete

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app=express();
+
+
+//config
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require("dotenv").config({
+        path:"backend/config/.env"
+    })
+}
+
+module.exports=app;
