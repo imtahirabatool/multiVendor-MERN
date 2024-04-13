f6da447bdcc3cb7fe740f14dba3c72e987fc9926
imtahirabatool <btahira183@gmail.com>
2024-04-13 19:25:54 +0500

    signUp page last commit

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/style";
-import {Link} from "react-router-dom"
-import {RxAvatar} from "react-icons/rx"
+import { Link } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
 function SignUp() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
-  const [avatar, setAvatar]=useState(null)
+  const [avatar, setAvatar] = useState(null);
 
-  const handleSubmit=()=>{
+  const handleSubmit = () => {
     console.log("bfubvf");
-  }
-  const handleFileInputChange=(e)=>{
-    const file=e.target.file(0);
-    setAvatar(file)
-  }
+  };
+  const handleFileInputChange = (e) => {
+    const file = e.target.file(0);
+    setAvatar(file);
+  };
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -106,47 +106,50 @@ function SignUp() {
             </div>
 
             <div className="">
+              <label
+                htmlFor="avatar"
+                className="black text-sm font-medium  text-gray-700"
+              ></label>
+              <div className="mt-2 flex items-center">
+                <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                  {avatar ? (
+                    <img
+                      src={URL.createObjectURL(avatar)}
+                      alt="avatar"
+                      className="h-full w-full object-cover rounded-full"
+                    />
+                  ) : (
+                    <RxAvatar className="h-8 w-8" />
+                  )}
+                </span>
                 <label
-                 htmlFor="avatar" 
-                className="black text-sm font-medium  text-gray-700">
-
+                  htmlFor="file-input"
+                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <span>Upload a File</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg,.jpeg,.png"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
                 </label>
-                <div className="mt-2 flex items-center">
-                    <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
-                        {
-                            avatar?
-                            (
-                                <img src={URL.createObjectURL(avatar)}
-                                 alt="avatar"
-                                  className="h-full w-full object-cover rounded-full" />
-                            ):(
-                                <RxAvatar className="h-8 w-8"/>
-                            )
-                        }
-                    </span>
-                    <label 
-                    htmlFor="file-input" 
-                    className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                        <span>Upload a File</span>
-                        <input type="file" name="avatar" id="file-input" accept=".jpg,.jpeg,.png"
-                        onChange={handleFileInputChange} 
-                        className="sr-only"/>
-                    </label>
-                </div>
+              </div>
             </div>
 
             <div className="">
-              <button 
-              type="submit"
-              className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 "
-              onSubmit={handleSubmit}>
+              <button
+                type="submit"
+                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 "
+                onSubmit={handleSubmit}
+              >
                 Submit
               </button>
             </div>
             <div className={`${styles.normalFlex} w-full`}>
-              <h4 >
-                Already have an account?
-              </h4>
+              <h4>Already have an account?</h4>
               <Link to="/login" className="text-blue-600 pl-2">
                 Sign In
               </Link>
