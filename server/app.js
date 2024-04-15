f70e573bda9bb436002655a6e36c41eb1df18c9a
imtahirabatool <btahira183@gmail.com>
2024-04-15 18:31:18 +0500

    backend setup complete

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const app=express();
+const express = require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
+const fileUpload = require("express-fileupload"); 
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 
-//config
-if(process.env.NODE_ENV!=="PRODUCTION"){
-    require("dotenv").config({
-        path:"backend/config/.env"
-    })
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload({ useTempFiles: true })); 
+
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "server/config/.env",
+  });
 }
 
-module.exports=app;
+// Error handling middleware
+app.use(ErrorHandler);
+
+module.exports = app;
