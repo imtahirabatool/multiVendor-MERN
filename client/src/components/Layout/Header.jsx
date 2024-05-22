5627c795f28fb52d3d7d2b289a73283def4f7f2f
imtahirabatool <btahira183@gmail.com>
2024-05-22 16:53:28 +0500

    Complete header section!

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,70 +1,167 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/style";
-import { productData } from "../../static/data";
-import { AiOutlineSearch } from "react-icons/ai";
+import { categoriesData, productData } from "../../static/data";
+import {
+  AiOutlineHeart,
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import { BiMenuAltLeft } from "react-icons/bi";
+import DropDown from "./DropDown";
+import { CgProfile } from "react-icons/cg";
+import Navbar from "./Navbar";
 
-const Header = () => {
+const Header = (activeHeading) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchData, setSearchData] = useState(null);
+  const [active, setActive] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
 
   const handleSearchChange = (e) => {
     const term = e.target.value;
     setSearchTerm(term);
 
-    const filterProducts =
-      productData &&
-      productData.filter((product) =>
-        product.name.toLowerCase().includes(term.toLowerCase())
-      );
+    const filterProducts = productData?.filter((product) =>
+      product.name.toLowerCase().includes(term.toLowerCase())
+    );
     setSearchData(filterProducts.length > 0 ? filterProducts : null);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 70) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={`${styles.section}`}>
-      <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
-        <div>
-          <Link to="/">
-            <img
-              src="https://shopo.quomodothemes.website/assets/images/logo.svg"
-              alt="logo"
+    <>
+      <div className={`${styles.section}`}>
+        <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+          <div>
+            <Link to="/">
+              <img
+                src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+                alt="logo"
+              />
+            </Link>
+          </div>
+          {/* Search box */}
+          <div className="w-[50%] relative">
+            <input
+              type="text"
+              placeholder="Search Products..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
             />
-          </Link>
-        </div>
-        {/* search box */}
-        <div className="w-[50%] relative">
-          <input
-            type="text"
-            placeholder="Search Products..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
-          />
-          <AiOutlineSearch
-            size={30}
-            className="absolute right-2 top-1.5 cursor-pointer"
-          />
-          {searchData && searchData.length > 0 && (
-            <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
-              {searchData.map((product) => (
-                <Link to={`/product/${product.id}`} key={product.id}>
-                  <div className="w-full flex items-center py-3">
-                    {product.image_Url && product.image_Url.length > 0 && (
-                      <img
-                        src={product.image_Url[0].url}
-                        alt=""
-                        className="w-[40px] h-[40px] mr-[10px]"
-                      />
-                    )}
-                    <h1>{product.name}</h1>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          )}
+            <AiOutlineSearch
+              size={30}
+              className="absolute right-2 top-1.5 cursor-pointer"
+            />
+            {searchData && (
+              <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
+                {searchData.map((product) => (
+                  <Link to={`/product/${product.id}`} key={product.id}>
+                    <div className="w-full flex items-center py-3">
+                      {product.image_Url && product.image_Url[0]?.url && (
+                        <img
+                          src={product.image_Url[0].url}
+                          alt={product.name}
+                          className="w-[40px] h-[40px] mr-[10px]"
+                        />
+                      )}
+                      <h1>{product.name}</h1>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
+          <div className={`${styles.button}`}>
+            <Link to="/seller">
+              <h1 className="text-[#fff] flex items-center">
+                Become Seller <IoIosArrowForward className="ml-1" />
+              </h1>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className={`${
+          active === true ? "shadow-sm fixed top-0 left-0 z-10" : ""
+        } transition hidden 800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}
+      >
+        <div
+          className={`${styles.section} relative flex items-center justify-between`}
+        >
+          {/* Categories */}
+          <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+            <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+            <button className="h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md">
+              All Categories
+            </button>
+            <IoIosArrowDown
+              size={20}
+              className="absolute right-2 top-4 cursor-pointer"
+              onClick={() => setDropDown(!dropDown)}
+            />
+            {dropDown ? (
+              <DropDown
+                categoriesData={categoriesData}
+                setDropDown={setDropDown}
+              />
+            ) : null}
+            {/*navitems*/}
+          </div>
+          <div className={`${styles.normalFlex}`}>
+            <Navbar active={activeHeading} />
+          </div>
+
+          <div className="flex">
+            <div className={`${styles.normalFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                <AiOutlineHeart size={30} color="rgb(255 255 255/ 83%)" />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 right text-white p-0 m-0 font-mono text-[12px] leading-tight text-center">
+                  0
+                </span>
+              </div>
+            </div>
+
+            <div className={`${styles.normalFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                <AiOutlineShoppingCart
+                  size={30}
+                  color="rgb(255 255 255/ 83%)"
+                />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 right text-white p-0 m-0 font-mono text-[12px] leading-tight text-center">
+                  1
+                </span>
+              </div>
+            </div>
+
+            <div className={`${styles.normalFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                <Link to="/login">
+                  <CgProfile size={30} color="rgb(255 255 255/ 83%)" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
